320b1f6b9c49823e353df1f483dab27f588b4960
elbasel <abdelrahman.elbasel@gmail.com>
2022-10-07 19:36:22 +0200

    added css reset and finsished button component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Button />
+        <Button title="button" variant="confirm" size="lg" disabled />
       </div>
     );
   }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
-
+import styles from "./Button.module.css";
 export default class Button extends Component {
   render() {
-    return <div>Button</div>;
+    const { onClick, variant, title, size, disabled } = this.props;
+
+    return (
+      <button
+        disabled={disabled}
+        onClick={onClick}
+        className={`${styles.button} ${styles[size]} ${styles[variant]}`}
+      >
+        {title}
+      </button>
+    );
   }
 }
