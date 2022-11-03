2dd428025878cf3ec7b9cdeabed248104cdaebbf
elbasel <abdelrahman.elbasel@gmail.com>
2022-11-03 23:19:55 +0200

    Remove unused var

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PureComponent } from "react";
+import React, { PureComponent } from "react";
 import { graphql } from "@apollo/client/react/hoc";
 import { gql } from "@apollo/client";
 
