97e70ac552b693cef7fb87a12ee27a177fb754a6
elbasel <abdelrahman.elbasel@gmail.com>
2022-11-08 08:11:23 +0200

    Add sass support

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import styles from "./Button.module.css";
+import styles from "./Button.module.scss";
 
 export default class Button extends Component {
   render() {
